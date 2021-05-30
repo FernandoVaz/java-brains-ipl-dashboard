b833e20e6940bf48f893e458584470d5f85ac890
FernandoVaz <fndmvaz@gmail.com>
2021-05-30 19:12:38 -0300

    Styling the pages

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -2,6 +2,9 @@ import { React, useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
+import { PieChart } from 'react-minimal-pie-chart';
+
+import './TeamPage.scss';
 
 export const TeamPage = () => {
 
@@ -24,10 +27,27 @@ export const TeamPage = () => {
   }
   return (
     <div className="TeamPage">
-        <h1>{team.teamName}</h1>
-        <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+        <div className="team-name-section">
+            <h1 className="team-name">{team.teamName}</h1>
+        </div>
+        
+        <div className="win-loss-section">
+            Win / Losses
+            <PieChart
+            data={[
+                {title : 'Wins', value: team.totalWins, color: 'rgb(30, 82, 30)'},
+                {title : 'Losses', value: team.totalMatches - team.totalWins, color: 'rgb(192, 55, 55)'}
+            ]}
+            />
+        </div>
+        
+        <div className="match-detail-section">
+            <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+        </div>
         {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match}/>)}
-
+        <div className="more-link">
+            <a href="#">More =></a>
+        </div>
     </div>
   );
 }
